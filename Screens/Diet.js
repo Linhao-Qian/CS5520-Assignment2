a47c81cda88c911f12886958b471a3e3e79a712a
Linhao-Qian <17768709606@163.com>
2024-09-30 18:05:43 -0700

    Added Diet and AddADietEntry screens

--- Screens/Diet.js
+++ Screens/Diet.js
@@ -2,13 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useContext } from 'react'
 import { Context } from '../helper/context';
 import { commonStyles } from '../helper/helper';
+import ItemsList from '../Components/ItemsList';
 
 export default function Diet() {
   const { theme, diet } = useContext(Context);
 
   return (
     <View style={[commonStyles.topContainer, commonStyles[theme]]}>
-      <Text>Diet</Text>
+      <ItemsList items={diet} />
     </View>
   )
 }
